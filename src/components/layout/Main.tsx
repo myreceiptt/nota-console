e417d64bbfb9c6d4d947779f7108931f7b1f6610
myreceiptt <nota@straight-line.org>
2023-08-16 00:41:54 +0700

    nota

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,4 +1,5 @@
 import { FC, ReactElement } from "react";
+import { Analytics } from '@vercel/analytics/react';
 
 interface MainProps {
   children: ReactElement;
@@ -8,6 +9,7 @@ const Main: FC<MainProps> = ({ children }) => {
   return (
     <main className="h-[74%] w-[99%] md:w-[88%] lg:w-[77%] xl:w-[74%]">
       {children}
+      <Analytics />
     </main>
   );
 };
